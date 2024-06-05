77112b5d1ba609b174f0c4f2696ace6896d2d4dc
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-05 09:46:14 +0900

    💄 add acordion

--- chakra-clock/src/App.tsx
+++ chakra-clock/src/App.tsx
@@ -1,4 +1,10 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
   Button,
   Card,
   CardBody,
@@ -11,7 +17,8 @@ import { Clock } from "./Clock";
 function App() {
   return (
     <>
-      <Card height={600} margin={6}>
+      <Card margin={6}>
+        {" "}
         <CardHeader>
           <Heading size="md"> Title1</Heading>
         </CardHeader>
@@ -22,6 +29,38 @@ function App() {
           <Button>button</Button>
         </CardFooter>
       </Card>
+      <Accordion allowToggle margin={6}>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <Clock />
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 2 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 }
