4c0d7acf492884f1c434a09c53a301eb3d1ac1b2
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-05 09:18:38 +0900

    💄 card clock

--- chakra-clock/src/App.tsx
+++ chakra-clock/src/App.tsx
@@ -1,9 +1,27 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+} from "@chakra-ui/react";
 import { Clock } from "./Clock";
 
 function App() {
   return (
     <>
-      <Clock />
+      <Card height={600} margin={6}>
+        <CardHeader>
+          <Heading size="md"> Title1</Heading>
+        </CardHeader>
+        <CardBody>
+          <Clock />
+        </CardBody>
+        <CardFooter>
+          <Button>button</Button>
+        </CardFooter>
+      </Card>
     </>
   );
 }
